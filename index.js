762727885e2e9d03b1bf4a5bc6e0b688e7871ac7
vic_che <chvi@yandex.ru>
2021-11-30 20:50:14 +0300

    add form

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRoute)
 app.use('/courses', coursesRoute)
